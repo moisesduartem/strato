c658845c56b64872f36d0a08c2a30b451c3be823
Moisés Mariano <moisesduartem@gmail.com>
2020-11-08 19:07:53 -0300

    controller example for generate cli

---
+++ config/cli/examples/controller.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace app\controllers\$entity_controller;
+
+function index()
+{
+    //... show all $entitys
+}
+
+function create()
+{
+    //... show a form to create a new $entity
+}
+
+function store()
+{
+    //... insert $entity on database
+}
+
+function show($id)
+{
+    //... show $entity
+}
+
+function edit($id)
+{
+    //... show view with a form to edit $entity
+    // return [];
+}
+
+function update($id)
+{
+    //... update $entity from database
+}
+
+function destroy($id)
+{
+    //... delete $entity from database
+}
