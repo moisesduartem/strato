11e88dfef2791659ba995473ca00bbf2509fe8ec
Moisés Mariano <moisesduartem@gmail.com>
2020-11-08 10:52:27 -0300

    create a route to test url parameters

--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,7 @@
 $routes = [
 
     ['method' => 'GET', 'uri' => '/', 'to' => 'user_controller#index'],
-    // ['method' => 'GET', 'uri' => '/hello/{name}', 'to' => 'user_controller#hello'],
+    ['method' => 'GET', 'uri' => '/hello/{name}', 'to' => 'user_controller#hello'],
     // ['method' => 'GET', 'uri' => '/users/{id}/books/{id}', 'to' => 'user_controller#book'],
 
 ];
