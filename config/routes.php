0532fcd06431f7a997bbe1c0034efe920773e905
Moisés Mariano <moisesduartem@gmail.com>
2020-11-08 14:54:54 -0300

    add a middleware to '/' GET route

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 
 $routes = [
 
-    ['method' => 'GET', 'uri' => '/', 'to' => 'user_controller#index'],
+    ['method' => 'GET', 'uri' => '/', 'to' => 'user_controller#index', 'middleware' => 'auth'],
     ['method' => 'GET', 'uri' => '/hello/{name}', 'to' => 'user_controller#hello'],
     ['method' => 'GET', 'uri' => '/users/{id}/books/{id}', 'to' => 'user_controller#book'],
 
