cfff5d770377b18d4caf3b642100d268513e81fb
Moisés Mariano <moisesduartem@gmail.com>
2020-11-08 10:19:44 -0300

    put a basic route for test

---
+++ config/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+$routes = [
+
+    ['method' => 'GET', 'uri' => '/', 'to' => 'user_controller#index'],
+    // ['method' => 'GET', 'uri' => '/hello/{name}', 'to' => 'user_controller#hello'],
+    // ['method' => 'GET', 'uri' => '/users/{id}/books/{id}', 'to' => 'user_controller#book'],
+
+];
+
+return $routes;
