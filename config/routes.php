536fbe66d02d23de5457bfdac4c5e12230603304
Moisés Mariano <moisesduartem@gmail.com>
2020-11-08 15:45:33 -0300

    create a route to search users

--- config/routes.php
+++ config/routes.php
@@ -3,8 +3,8 @@
 $routes = [
 
     ['method' => 'GET', 'uri' => '/', 'to' => 'user_controller#index', 'middleware' => 'auth'],
+    ['method' => 'GET', 'uri' => '/users/{id}', 'to' => 'user_controller#show'],
     ['method' => 'GET', 'uri' => '/hello/{name}', 'to' => 'user_controller#hello'],
-    ['method' => 'GET', 'uri' => '/users/{id}/books/{id}', 'to' => 'user_controller#book'],
 
 ];
 
