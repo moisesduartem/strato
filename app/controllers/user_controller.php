85d6f5839f756011781159a35c92d1a08d4d7e31
Moisés Mariano <moisesduartem@gmail.com>
2020-11-08 11:40:16 -0300

    fix: don't need to import view anymore cause now have a convention

--- app/controllers/user_controller.php
+++ app/controllers/user_controller.php
@@ -2,16 +2,14 @@
 declare(strict_types=1);
 
 namespace app\controllers\user_controller;
-use function config\helper\view;
 
 function index()
 {
-    return view('users.index');
 }
 
 function hello($name)
 {
-    echo 'hello ' . $name;
+    
 }
 
 function book($user_id, $book_id)
